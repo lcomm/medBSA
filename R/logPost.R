b2181f3deb211414ee99d97acf13d59086de7eef
lcomm <leah.comment@gmail.com>
2015-08-09 19:17:36 -0400

    Make log-posterior calculations possible for categorical variables

---
+++ R/logPost.R
@@ -0,0 +1,21 @@
+#' Function to return the log-posterior for a parameter set
+#' 
+#' This function relies on existing lists in the environment (priors, Xmats) to 
+#' calculate the log-posterior of those parameter coefficients.
+#' 
+#' @param v String containing the variable of interest
+#' @param par Parameter vector of regression coefficients
+#' 
+logPost <- function(v, par) {
+    
+    if (isBinary(fam[[v]])) {
+        #Binary
+        logPostBin(v, par)
+        
+    } else if (isMultinomial(fam[[v]])) {
+        #Categorical
+        logPostMultinom(v, par)
+        
+    } else { stop("Likelihood form not recognized by logPost function!") }
+        
+}
