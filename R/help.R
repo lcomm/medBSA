2f2edfaaca32c0a5704eae31b7fe8c0ffadde96e
lcomm <leah.comment@gmail.com>
2016-08-16 17:21:59 -0400

    Starting merge process for Rcpp files

---
+++ R/help.R
@@ -0,0 +1,8 @@
+#' medBSA: a package for Bayesian sensitivity analysis for mediation
+#'
+#' @docType package
+#' @name medBSA
+#' @import RcppArmadillo
+#' @importFrom Rcpp evalCpp
+#' @useDynLib medBSA
+NULL
